05e0ee86435c631b5d4eba40583f8d0a40641766
João Nuno Mota <jnuno.carrilho@gmail.com>
2020-02-16 22:39:11 +0000

    Add BaseAccordian component

---
+++ src/components/BaseAccordian.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div id="header" @click="toggleIsHidden">
+      <slot name="header"></slot>
+    </div>
+    <div id="body" :hidden="isHidden">
+      <slot name="body"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "base-accordian",
+  props: {
+    open: Boolean
+  },
+  data() {
+    return {
+      isHidden: !this.open
+    };
+  },
+  methods: {
+    toggleIsHidden() {
+      this.isHidden = !this.isHidden;
+    }
+  }
+};
+</script>
