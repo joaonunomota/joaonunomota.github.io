af814e515733fc6937ab2d6164cfc6b8ca6478b2
João Nuno Mota <jnuno.carrilho@gmail.com>
2020-02-20 22:31:07 +0000

    Remove Facebook and Instagram social links

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
   <div id="app">
     <h1>Jo&atilde;o Nuno Mota</h1>
-    <p>Coming soon.</p>
     <div>
-      <facebook-social-link class="social-icon" />
       <github-social-link class="social-icon" />
-      <instagram-social-link class="social-icon" />
       <linked-in-social-link class="social-icon" />
     </div>
   </div>
 </template>
 <script>
-import FacebookSocialLink from "./components/FacebookSocialLink";
 import GithubSocialLink from "./components/GithubSocialLink";
-import InstagramSocialLink from "./components/InstagramSocialLink";
 import LinkedInSocialLink from "./components/LinkedInSocialLink";
 
 export default {
   components: {
-    FacebookSocialLink,
     GithubSocialLink,
-    InstagramSocialLink,
     LinkedInSocialLink
   }
 };
