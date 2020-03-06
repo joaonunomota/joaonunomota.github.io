611c29ba31ffd669e778623e958770ce2880b0e2
João Nuno Mota <jnuno.carrilho@gmail.com>
2020-03-06 08:36:10 +0000

    Add and format content in accordians

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,30 @@
     <div id="wrapper">
       <h1>Jo&atilde;o Nuno Mota</h1>
       <div>
-        A full-stack web developer focused on C# and JavaScript. Used to
-        delivering on multiple projects in a fast-paced agile work environment.
-        Eager to continuously learn and improve on all aspects of software
-        development.
-        <simple-header-accordion header="Education"></simple-header-accordion>
+        <p>
+          A full-stack web developer focused on C# and JavaScript. Used to
+          delivering on multiple projects in a fast-paced agile work
+          environment. Eager to continuously learn and improve on all aspects of
+          development.
+        </p>
+        <simple-header-accordion header="Core Skills">
+          <div class="row left-align">
+            <h4>Proficient</h4>
+            <p>
+              Asp.Net Core Razor Pages & Web API, Asp.Net MVC5 & Web Pages, C#,
+              EF Core, HTML5, Java, JavaScript, jQuery, SQL, TypeScript, Vue.js
+            </p>
+            <h4>Familiar</h4>
+            <p>
+              Asp.Net Core MVC, Azure, CSS3, Knockout.js, PowerShell, Python, R,
+              WCF
+            </p>
+          </div>
+        </simple-header-accordion>
         <simple-header-accordion header="Career">
           <div class="row space-between">
             <div class="left-align">
-              .Net Developer
+              <strong>.Net Developer</strong>
               <br />
               InfoTrack UK -
               <a href="https://www.infotrack.co.uk">infotrack.co.uk</a>
@@ -38,7 +53,7 @@
 
           <div class="row space-between">
             <div class="left-align">
-              Junior Software Developer
+              <strong>Junior Software Developer</strong>
               <br />
               DCSL Software Ltd. -
               <a href="https://www.dcslsoftware.com">dcslsoftware.com</a>
@@ -67,10 +82,78 @@
               </li>
             </ul>
           </div>
+
+          <div class="row space-between">
+            <div class="left-align">
+              <strong>Web Developer</strong>
+              <br />
+              Augusto de Oliveira Ferreira Lda. -
+              <a href="https://www.aof.pt">aof.pt</a>
+            </div>
+            <div class="right-align">
+              June 2015 - September 2016
+            </div>
+          </div>
+          <div class="row left-align">
+            <p>
+              A construction company specializing in the conservation and
+              restoration of heritage buildings.
+            </p>
+            <ul>
+              <li>
+                Self-taught C# and Asp.Net Web Pages to implement a portfolio
+                website (aof.pt), including a content management system to
+                create and edit multilingual pages containing user media.
+              </li>
+            </ul>
+          </div>
         </simple-header-accordion>
-        <simple-header-accordion
-          header="Miscellaneous"
-        ></simple-header-accordion>
+        <simple-header-accordion header="Education">
+          <div class="row space-between">
+            <div class="left-align">
+              <strong>Computer Science MSc (2:1)</strong>
+              <br />
+              Royal Holloway, University of London
+            </div>
+            <div class="right-align">
+              2014 - 2018
+            </div>
+          </div>
+          <div class="row left-align">
+            <p>
+              Modules Included:
+              <br />
+              <br />
+              Large Scale Data Storage and Processing, Machine Learning,
+              Security Management, Applications of Cryptography, Computational
+              Finance
+            </p>
+            <ul>
+              <li>
+                Awarded “Best Team Project” prize for designing and developing
+                an attendance management system using Laravel, Java, HTML5 and
+                Bootstrap.
+              </li>
+              <li>
+                Implemented the following machine learning algorithms in Java
+                for Full Year Project:
+                <ul>
+                  <li>
+                    Weighted k-Nearest Neighbor for classification and
+                    regression.
+                  </li>
+                  <li>
+                    Support Vector Machines with variant to support 2+ classes.
+                  </li>
+                  <li>
+                    AdaBoost using Classification and Regression Tree as weak
+                    learner.
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </simple-header-accordion>
       </div>
       <div>
         <github-social-link class="social-icon" />
