486ef7104d529c3b3ed672b1f9ce32cfc0b3e03c
João Nuno Mota <jnuno.carrilho@gmail.com>
2020-03-11 07:28:13 +0000

    Add social links to not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -3,9 +3,25 @@
     <div id="wrapper">
       <h1>Jo&atilde;o Nuno Mota</h1>
       <h2>Page Not Found</h2>
+      <div>
+        <github-social-link class="social-icon" />
+        <linked-in-social-link class="social-icon" />
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+import GithubSocialLink from "../components/GithubSocialLink";
+import LinkedInSocialLink from "../components/LinkedInSocialLink";
+
+export default {
+  components: {
+    GithubSocialLink,
+    LinkedInSocialLink
+  }
+};
+</script>
 
 <style lang="scss">
 @import "../assets/css/site.scss";
@@ -23,5 +39,11 @@
   width: 100%;
   max-width: 720px;
   padding: 0 2rem 0 2rem;
+}
+
+.social-icon {
+  display: inline-block;
+  margin: 12px;
+  height: 60px;
 }
 </style>
